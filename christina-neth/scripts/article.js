8c8bac5186fe8bd33ce8f68c613e615ef3d3fb4c
Christina Gislason <mkcgislason@gmail.com>
2018-09-07 10:54:41 -0700

    articles rendering with handlebars

--- christina-neth/scripts/article.js
+++ christina-neth/scripts/article.js
@@ -14,6 +14,22 @@ function Article (rawDataObj) {
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
+  const articleScript = $('#article-template').html();
+  const theArticle = Handlebars.compile(articleScript);
+
+  const articleContext={
+    'title': this.title,
+    'category': this.category,
+    'author': this.author,
+    'authorUrl': this.authorUrl,
+    'publishedOn': this.publishedOn,
+    'body': this.body,
+  };
+
+  const theCompiledArticle = theArticle(articleContext);
+
+  $('#article-placeholder').append(theCompiledArticle);
+
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
