8c3d999047a611d1f066029aa9c826f00e3ef7c2
neth <nethwebdev@gmail.com>
2018-09-07 11:36:29 -0700

    finished article.js, refactored articleView functions

--- christina-neth/scripts/article.js
+++ christina-neth/scripts/article.js
@@ -12,30 +12,16 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  // DONE! Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
   const articleScript = $('#article-template').html();
   const theArticle = Handlebars.compile(articleScript);
-
-  const articleContext={
-    'title': this.title,
-    'category': this.category,
-    'author': this.author,
-    'authorUrl': this.authorUrl,
-    'publishedOn': this.publishedOn,
-    'body': this.body,
-  };
-
-  const theCompiledArticle = theArticle(articleContext);
-
-  $('#article-placeholder').append(theCompiledArticle);
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
@@ -46,12 +32,21 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  // DONE! Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  const articleContext = {
+    'title': this.title,
+    'category': this.category,
+    'author': this.author,
+    'authorUrl': this.authorUrl,
+    'publishStatus': this.publishStatus,
+    'body': this.body,
+  };
 
+  return theArticle(articleContext);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// Arrow functions that take only a single argument don't need parentheses, but arrow functions with more than one (or less than one) argument do.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
